efa126700bebbaca5e19c2e983798db2f247a3f6
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2024-06-04 13:18:59 -0300

    removed node modules

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', function(req, res) {
     res.send('Hello, world!');
 })
+app.get('/assessment', function(req, res) {
+    res.sendFile(__dirname + '/assessment.html');
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
