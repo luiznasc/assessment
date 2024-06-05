c9b6ee807ce05a42c7ae548b0a1b0088ce6265d4
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2024-06-04 23:09:03 -0300

    initial search attempt

--- scraper.js
+++ scraper.js
@@ -2,10 +2,28 @@ const axios = require('axios');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-
-axios.get('amazon.com')
-.then(function (response) {
-    console.log(response);
+const search = 'tupperware'
+let products;
+let ratings;
+let numberOfRatings;
+axios({
+    method: 'get',
+    url: `https://www.amazon.com/s?k=${search}`,
 })
+.then(function (response) {
+    const dom = new JSDOM(response.data);
+    // products = dom.window.document.getElementsByTagName()
 
-const dom = new JSDOM();
+    ratings = dom.window.document.getElementsByClassName('a-icon-star-small');
+
+    // Walking through the ratings within the product list
+    for (var i in ratings) {
+        // console.log(ratings[i].textContent);
+    };
+
+    numberOfRatings = dom.window.document.querySelectorAll("[data-component-type='s-search-result']");
+    for (var i in numberOfRatings) {
+        console.log(numberOfRatings[i], i);
+    }
+})
+.catch(err => console.error(err));
