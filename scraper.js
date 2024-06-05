93f0dd8bc0fe6c26a4e094dff110a2191f625aa6
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2024-06-05 14:27:45 -0300

    found ratings, review numbers and titles

--- scraper.js
+++ scraper.js
@@ -5,25 +5,43 @@ const { JSDOM } = jsdom;
 const search = 'tupperware'
 let products;
 let ratings;
-let numberOfRatings;
+let items;
 axios({
+    // The method used in the request:
     method: 'get',
+    // The products search url:
     url: `https://www.amazon.com/s?k=${search}`,
+    // This is just so amazon doesn't flag you as a bot/DoS system, setting the User-Agent.
+    headers: { 'User-Agent': 'Mozilla/5.0' } , 
 })
-.then(function (response) {
-    const dom = new JSDOM(response.data);
-    // products = dom.window.document.getElementsByTagName()
+.then(function (response) { // Response manipulation
 
-    ratings = dom.window.document.getElementsByClassName('a-icon-star-small');
+    const dom = new JSDOM(response.data); //
+    items = dom.window.document.querySelectorAll("[data-component-type='s-search-result']");
 
-    // Walking through the ratings within the product list
-    for (var i in ratings) {
-        // console.log(ratings[i].textContent);
-    };
+    for (var i in items) {
+        if (i === 'item') {
+            break;
+        }
+        console.log('\n');
+        let titleContent = items[i].getElementsByClassName("s-underline-text")[0].getElementsByTagName('span')
+        // This flow control is to merge the spans, since sometimes the text content is divided in two.
+        let details;
+        if (titleContent.length > 1) {
+            details = " " + titleContent[1].textContent
+        } else {
+            details = '';
+        }
+        titleContent = titleContent[0].textContent + details;
+        console.log('TITLE: '+ titleContent);
 
-    numberOfRatings = dom.window.document.querySelectorAll("[data-component-type='s-search-result']");
-    for (var i in numberOfRatings) {
-        console.log(numberOfRatings[i], i);
+        let rating = items[i].getElementsByClassName("a-icon-star-small")[0].textContent;
+        let numberOfReviews = items[i].querySelectorAll("[data-csa-c-content-id='alf-customer-ratings-count-component']")[0].textContent;
+        
+        console.log('Rating: ' + rating);
+        console.log('Number of Reviews: ' + numberOfReviews);
+        
     }
+    
 })
 .catch(err => console.error(err));
