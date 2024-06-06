0d995506f293a3b475050818290b3306398009e1
luiznasc <luiz.nascimen@ccc.ufcg.edu.br>
2024-06-06 19:37:52 -0300

    adding scraper dependency and api endpoint

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const Search = require("./scraper.js");
+
+app.get("/", function (req, res) {
+  res.send("<p>Information on <a href=assessment>assessment</a></p>");
+});
+app.get("/assessment", function (req, res) {
+  res.sendFile(__dirname + "/assessment.html");
+});
+
+app.get("/api/scrape", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const keyword = req.query.keyword;
+  if (!keyword) {
+    return res
+      .status(400)
+      .json({ error: "Missing keyword. Please provide one." });
+  };
+
+  Search(keyword)
+  .then(response => {
+    console.log(response);
+    res.status(200).json(response);
+  })
+
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
--- scraper.js
+++ scraper.js
@@ -1,54 +1,79 @@
-const axios = require('axios');
-const jsdom = require('jsdom');
+const axios = require("axios");
+const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const search = 'dish washer'
-let products;
-let ratings;
-let items;
-axios({
-    // The method used in the request:
-    method: 'get',
-    // The products search url:
-    url: `https://www.amazon.com/s?k=${search}`,
-    // This is just so amazon doesn't flag you as a bot/DoS system, setting the User-Agent.
-    headers: { 'User-Agent': 'Mozilla/5.0' } , 
-})
-.then(function (response) { // Response manipulation
+async function Search(keyword) {
+  try {
+    const response = await axios({
+      // The method used in the request:
+      method: "get",
+      // The products search url:
+      url: `https://www.amazon.com/s?k=${keyword}`,
+      // This is just so amazon doesn't flag you as a bot, setting the User-Agent.
+      headers: { "User-Agent": "Mozilla/5.0" },
+    });
 
+    // Response transformation and filtering
     const dom = new JSDOM(response.data); //
-    items = dom.window.document.querySelectorAll("[data-component-type='s-search-result']");
+    const items = dom.window.document.querySelectorAll(
+      "[data-component-type='s-search-result']"
+    );
 
-    for (var i in items) {
-        if (i === 'item') {
-            break;
-        }
-        console.log('\n');
-        console.log(`${i} i`);
+    // Scraping and building the final result
+    let results = {};
+    for (var i = 0; i < items.length; i++) {
+      // This is to prevent the loop from spilling off of the json results
+      if (i === "item") {
+        break;
+      }
+      // Title scraping
+      let titleContent = items[i]
+        .getElementsByClassName("s-underline-text")[0]
+        .getElementsByTagName("span");
 
-        let image = items[i].getElementsByClassName('s-image-optimized-rendering')[0].src;
-        console.log(image);
+      // This flow control allows merging the title or description spans,
+      // since sometimes the text content is divided in two.
+      let details;
+      if (titleContent.length > 1) {
+        details = " " + titleContent[1].textContent;
+      } else {
+        details = "";
+      }
+      titleContent = titleContent;
+      titleContent = titleContent[0].textContent + details;
 
-        let titleContent = items[i].getElementsByClassName("s-underline-text")[0].getElementsByTagName('span')
-        // This flow control is to merge the spans, since sometimes the text content is divided in two.
-        let details;
-        if (titleContent.length > 1) {
-            details = " " + titleContent[1].textContent
-        } else {
-            details = '';
-        }
-        titleContent = titleContent[0].textContent + details;
-        console.log('TITLE: '+ titleContent);
+      // Ratings(stars out of 5)
+      let rating = items[i].getElementsByClassName("a-icon-star-small")[0];
+      // Making sure that there are reviews
+      rating = rating ? rating.textContent : "";
 
-        // Ratings(stars out of 5)
-        let rating = items[i].getElementsByClassName("a-icon-star-small")[0].textContent;
-        // Review count
-        let numberOfReviews = items[i].querySelectorAll("[data-csa-c-content-id='alf-customer-ratings-count-component']")[0].textContent;
-        
-        console.log('Rating: ' + rating);
-        console.log('Number of Reviews: ' + numberOfReviews);
-        
+      // Review count scraping with query selector, since most other choices
+      // were either too long or yielding false positives.
+      let numberOfReviews = items[i].querySelectorAll(
+        "[data-csa-c-content-id='alf-customer-ratings-count-component']"
+      )[0];
+      // Making sure that number of reviews is present
+      numberOfReviews = numberOfReviews ? numberOfReviews.textContent : "";
+
+      // Image scrape
+      let image = items[i].getElementsByClassName(
+        "s-image-optimized-rendering"
+      )[0].src;
+
+      // Basic json object building
+      results[i] = {
+        "product title": titleContent,
+        rating: rating,
+        "number of reviews": numberOfReviews,
+        image: image,
+      };
     }
-    
-})
-.catch(err => console.error(err));
+
+    return results;
+  } catch (error) {
+    // Simple error handling
+    return { error: error.message };
+  }
+}
+
+module.exports = Search;
